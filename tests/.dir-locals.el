eab92337d1d2d3c10886f2b11492b4a63b797c66
Chris MacMackin <cmacmackin@gmail.com>
2016-04-21 11:16:38 +0100

    Added .dir-locals.el to configure fly-check

---
+++ tests/.dir-locals.el
@@ -0,0 +1,17 @@
+((nil . ((eval . (set (make-local-variable 'my-project-path)
+                      (file-name-directory
+                       (let ((d (dir-locals-find-file ".")))
+                         (if (stringp d) d (car d))))))
+         (eval . (message "Project directory set to `%s'." my-project-path))
+         (eval . (setq flycheck-gfortran-include-path
+              (mapcar (lambda (p) (expand-file-name p my-project-path)) 
+                          '("~/.local/include/" 
+	                    "../mod"
+		            "../factual/mod"
+			    "./mod"))))
+         (eval . (message "Include directories set to `%s'." flycheck-gfortran-include-path))
+	 (eval . (setq flycheck-gfortran-args 
+		     (concat "-J" 
+		         (expand-file-name '"./mod" my-project-path))))
+         (eval . (message "Gfortran arguments set to `%s'." flycheck-gfortran-args))
+)))
