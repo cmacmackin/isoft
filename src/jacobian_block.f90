f0fd17804ffeeb1c55f9d1703358a5330cfb5c1c
Chris MacMackin <cmacmackin@gmail.com>
2016-12-31 16:52:23 -0400

    Added modules for preconditioning.

---
+++ src/jacobian_block.f90
@@ -0,0 +1,303 @@
+!
+!  jacobian_block.f90
+!  This file is part of ISOFT.
+!  
+!  Copyright 2016 Chris MacMackin <cmacmackin@gmail.com>
+!  
+!  This program is free software; you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation; either version 2 of the License, or
+!  (at your option) any later version.
+!  
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!  
+!  You should have received a copy of the GNU General Public License
+!  along with this program; if not, write to the Free Software
+!  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+!  MA 02110-1301, USA.
+!  
+
+module jacobian_block_mod
+  !* Author: Christopher MacMackin
+  !  Date: December 2016
+  !  License: GPLv3
+  !
+  ! Provides a derived type which is useful for representing blocks in
+  ! a Jacobian matrix. This follows the abstract calculus pattern,
+  ! providing operators for matrix multiplication and for solving the
+  ! linear(ised) system. See the documentation for the
+  ! [[jacobian_block(type)]] type for more details.
+  !
+  use iso_fortran_env, only: r8 => real64
+  use factual_mod, only: scalar_field, vector_field
+  use f95_lapack, only: la_gtsvx
+  use logger_mod, only: logger => master_logger
+  implicit none
+  private
+
+  integer, parameter :: no_extra_derivative = -1
+  
+  type, public :: jacobian_block
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! A data type representing a submatrix of a
+    ! Jacobian. Specifically, it represents the commonly occurring
+    ! operation $$ \frac{\partial F}{\partial x_i} + F\Delta_i $$,
+    ! where \( \Delta_i \) is the differentiation operator in the
+    ! \(i\)-direction. Optionally, there can be an additional
+    ! differentiation operator on the right-hand-side of this.
+    !
+    ! This data type is useful when constructing a preconditioner. A
+    ! preconditioner, \(A\), should approximate the inverse Jacobian of
+    ! a problem being solved. Rather than directly computing the
+    ! inverse Jacobian, it is more efficient to approximate it. If
+    ! \(d\) is the vector being preconditioned, and \(z\) is the
+    ! result of applying the preconditioner, then $$ z = J^{-1}d
+    ! \Rightarrow Jz = d$$. Thus, the preconditioner can be applied
+    ! by approximately solving this system for \(z\). Linearising
+    ! \(J\), this system can be solved efficiently using Picard
+    ! iteration.
+    !
+    ! Say that the Jacobian is an \(n\times n\) system of these blocks
+    ! (each labeled as \(J_{j,k}) and that the vector being
+    ! preconditioned constists of \(n\) scalar fields (\(d_j\)). Then
+    ! \(m^{th}\) estimate of the solution for the \(j^{th}\)
+    ! field in the preconditioned vector (\(z^m_j\)) is the solution
+    ! to $$ J_{j,j}z^m_j = d_j - \sum_{\substack{k=1\ k\ne j}}^n
+    ! J{j,k}z^{m-1}_k. $$ Depending on the type of fields being used
+    ! and the direction in which derivatives are being taken,
+    ! \(J_{j,j}\) may be tridiaganol, meaning it can be solved
+    ! efficiently. <!--Otherwise, it can be approximated that $$
+    ! J_{j,j}z^m_j = \left(\frac{\partial F}{\partial x_i} +
+    ! F\Delta_i\right)z^{m} \simeq \frac{\partial F}{\partial
+    ! x_i}z^m_j + F\frac{\partial z^{m-1}_j}{\partial x_i}. $$ The
+    ! first term in this approximation corresponds to a diagonal
+    ! matrix (which can be solved trivially), while the second term is
+    ! known and can be subtracted from the right-hand-side of the
+    ! linear system.-->
+    !
+    ! For this purpose, a type-bound multiplication operator is
+    ! provided for the Jacobian block type, which can be used when
+    ! evaluating the right hand side of the linear system. There is
+    ! also a `solve_for` method, which takes as an argument the field
+    ! representing the right hand side of the system. <!--Before
+    ! applying the latter operator, the `update_estimate` method must
+    ! be called. This allows for the term involving derivative of the
+    ! current estimate of the solution to be subtracted from the
+    ! right-hand-side of the linear system, in cases where the system
+    ! can not be expressed as a tridiagonal matrix.-->
+    !
+    private
+    integer                             :: direction
+      !! The direction in which any derivatives are taken.
+    integer                             :: extra_derivative = no_extra_derivative
+      !! The direction in which to apply a differentiation on the
+      !! right-hand-side of the Jacobian block operator. Defaults
+      !! none.
+    class(scalar_field), allocatable    :: contents
+      !!The value, \(A\), to which the Jacobian block operation is
+      !!beiing applied.
+    class(scalar_field), allocatable    :: derivative
+      !! The cached derivative of `contents`
+    real(r8), dimension(:), allocatable :: diagonal
+      !! The diagonal of the tridiagonal matrix representation of this
+      !! block.
+    real(r8), dimension(:), allocatable :: super_diagonal
+      !! The super-diagonal of the tridiagonal matrix representation
+      !! of this block.
+    real(r8), dimension(:), allocatable :: sub_diagonal
+      !! The sub-diagonal of the tridiagonal matrix representation of
+      !! this block.
+    real(r8), dimension(:), allocatable :: l_multipliers
+      !! Multipliers defining the L matrix in the LU factorisation of
+      !! the tridiagonal matrix representation of this block.
+    real(r8), dimension(:), allocatable :: u_diagonal
+      !! The diagonal of the U matrix in the LU factorisation of
+      !! the tridiagonal matrix representation of this block.
+    real(r8), dimension(:), allocatable :: u_superdiagonal1
+      !! The first superdiagonal of the U matrix in the LU
+      !! factorisation of the tridiagonal matrix representation of
+      !! this block.
+    real(r8), dimension(:), allocatable :: u_superdiagonal2
+      !! The second superdiagonal of the U matrix in the LU
+      !! factorisation of the tridiagonal matrix representation of
+      !! this block.
+    integer, dimension(:), allocatable  :: pivots
+      !! Pivot indicies from the LU factorisation of the tridiagonal
+      !! matrix representation of this block.
+  contains
+    private
+    procedure :: jacobian_block_multiply
+    generic, public :: operator(*) => jacobian_block_multiply
+    procedure, public :: solve_for => jacobian_block_solve
+  end type jacobian_block
+
+  interface jacobian_block
+    module procedure constructor
+  end interface jacobian_block
+
+contains
+
+  function constructor(source_field, direction, extra_derivative) result(this)
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! Build a block in a Jacobian matrix, with the form
+    ! $$\frac{\partial F}{\partial x_i} + F\Delta_i$$, where \(F\) is
+    ! a scalar field and \(\Delta_i\) is the differentiation operator
+    ! in the \(i\)-direction. Additionally, a further differentiation
+    ! operator may be added to the right hand side of this matrix block.
+    !
+    class(scalar_field), intent(in) :: source_field
+      !! A scalar field (\(F\)) making up this block of the Jacobian
+    integer, intent(in)             :: direction
+      !! The direction in which field derivatives are taken.
+    integer, intent(in), optional   :: extra_derivative
+      !! If present, specifies the direction of a differentiation
+      !! operator to be added to the right hand side of this matrix
+      !! block.
+    type(jacobian_block)            :: this
+      !! A new Jacobian block
+    allocate(this%contents, source=source_field)
+    allocate(this%derivative, mold=this%contents)
+    this%derivative = this%contents%d_dx(this%direction)
+    this%direction = direction
+    if (present(extra_derivative)) this%extra_derivative = extra_derivative
+  end function constructor
+
+  function jacobian_block_multiply(this, rhs) result(product)
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! Provides a matrix multiplication operator between a Jacobian
+    ! block and a scalar field (which corresponds to a state vector).
+    !
+    class(jacobian_block), intent(in) :: this
+    class(scalar_field), intent(in)   :: rhs
+      !! A field corresponding to a state vector being multiplied by
+      !! the Jacobian block.
+    class(scalar_field), allocatable  :: product
+    class(scalar_field), allocatable  :: tmp
+    allocate(product, mold=this%contents)
+    if (this%extra_derivative==no_extra_derivative) then
+      product = this%derivative * rhs + this%contents * rhs%d_dx(this%direction)
+    else
+      allocate(tmp, mold=rhs)
+      tmp = rhs%d_dx(this%extra_derivative)
+      product = this%derivative * tmp + this%contents * tmp%d_dx(this%direction)
+    end if
+  end function jacobian_block_multiply
+
+  function jacobian_block_solve(this, rhs) result(solution)
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! Solves the linear(ised) system represented by this Jacobian
+    ! block, for a given right hand side state vector (represented by
+    ! a scalar field).
+    !
+    ! @Warning Currently this is only implemented for a 1-D field. The
+    ! extra derivative must be in the direction along which the field
+    ! varies.
+    !
+    class(jacobian_block), intent(inout) :: this
+    class(scalar_field), intent(in)      :: rhs
+      !! The right hand side of the linear(ised) system.
+    class(scalar_field), allocatable     :: solution
+    real(r8), dimension(:), allocatable  :: sol_vector
+
+    character(len=51), parameter :: error_format = '("Tridiagonal matrix '// &
+         'solver returned with flag",i0)'
+    character(len=77), parameter :: success_format = '("Tridiagonal matrix '// &
+         'solver returned with estimated condition number ",es8.5)'
+    integer                                   :: flag, n
+    real(r8)                                  :: forward_err, &
+                                                 backward_err, &
+                                                 condition_num
+    character(len=1)                          :: factor
+    character(len=:), allocatable             :: msg
+    class(vector_field), allocatable          :: grid
+    logical                                   :: use_cached
+    class(scalar_field), allocatable, save    :: cached_field_type
+    real(r8), dimension(:), allocatable, save :: cached_grid_spacing
+    real(r8), dimension(:), allocatable       :: cont, deriv
+    
+    allocate(sol_vector(rhs%raw_size()))
+    ! Construct tridiagonal matrix for this operation, if necessary
+    if (.not. allocated(this%pivots)) then
+      call logger%debug('jacobian_block_solve','Constructing tridiagonal matrix.')
+      factor = 'N'
+      n = this%contents%raw_size()
+      ! Try to use cached copy of inverse grid spacing, if available and suitable 
+      if (allocated(cached_field_type)) then
+        use_cached = same_type_as(this%contents, cached_field_type) .and. &
+                     n==size(cached_grid_spacing)
+      else
+        use_cached = .false.
+      end if
+      ! Construct an array containing the distance between grid
+      ! points, if necessary
+      if (.not. use_cached) then
+        deallocate(cached_field_type)
+        allocate(cached_field_type, mold=this%contents)
+        call this%contents%allocate_vector_field(grid)
+        grid = this%contents%grid_spacing()
+        cached_grid_spacing = 0.5_r8/grid%raw()
+        cached_grid_spacing(1) = 2 * cached_grid_spacing(1)
+        cached_grid_spacing(n) = 2 * cached_grid_spacing(n)        
+      end if
+      cont = this%contents%raw()
+      if (this%extra_derivative == no_extra_derivative) then
+        ! Create the tridiagonal matrix when there is no additional
+        ! differentiation operator on the RHS
+        this%diagonal = this%derivative%raw()
+        this%diagonal(1) = this%diagonal(1) - cached_grid_spacing(1)
+        this%diagonal(n) = this%diagonal(n) + cached_grid_spacing(n)
+        this%super_diagonal = cont(1:n-1) * cached_grid_spacing(1:n-1)
+        this%sub_diagonal = -cont(2:n) * cached_grid_spacing(2:n)
+      else if (this%extra_derivative == this%direction) then
+        ! Create the tridiagonal matrix when the additional
+        ! differentiation operator on the RHS operates in the same
+        ! direction as the derivative of the contents.
+        deriv = this%derivative%raw()
+        this%diagonal = -2 * deriv * cached_grid_spacing
+        this%diagonal(1) = this%diagonal(1) - deriv(1) * cached_grid_spacing(1)
+        this%diagonal(n) = this%diagonal(n) + deriv(n) * cached_grid_spacing(n)
+        this%super_diagonal = (cont(1:n-1) + deriv(1:n-1)) * cached_grid_spacing(1:n-1)
+        this%sub_diagonal = (cont(2:n) - deriv(2:n)) * cached_grid_spacing(2:n)
+      else
+        error stop('Currently no support for extra derivatives other than '// &
+                   'in the 1-direction')
+      end if
+      ! Allocate the arrays used to hold the factorisation of the
+      ! tridiagonal matrix
+      allocate(this%l_multipliers(n-1))
+      allocate(this%u_diagonal(n))
+      allocate(this%u_superdiagonal1(n-1))
+      allocate(this%u_superdiagonal2(n-2))
+      allocate(this%pivots(n))
+    else
+      factor = 'F'
+    end if
+    call la_gtsvx(this%sub_diagonal, this%diagonal, this%super_diagonal,      &
+                  rhs%raw(), sol_vector, this%l_multipliers, this%u_diagonal, &
+                  this%u_superdiagonal1, this%u_superdiagonal2, this%pivots,  &
+                  factor, 'N', forward_err, backward_err, condition_num,      &
+                  flag)
+    if (flag/=0) then
+      write(msg,error_format) flag
+      call logger%error('jacobian_block_solve',msg)
+    else
+      write(msg,success_format) condition_num
+      call logger%debug('jacobian_block_solve',msg)
+    end if
+    allocate(solution, mold=rhs)
+    call solution%assign_meta_data(rhs)
+    call solution%set_from_raw(sol_vector)
+  end function jacobian_block_solve
+
+end module jacobian_block_mod
---
+++ src/preconditioner.F90
@@ -0,0 +1,191 @@
+!
+!  preconditioner.f90
+!  This file is part of ISOFT.
+!  
+!  Copyright 2016 Chris MacMackin <cmacmackin@gmail.com>
+!  
+!  This program is free software; you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation; either version 2 of the License, or
+!  (at your option) any later version.
+!  
+!  This program is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!  
+!  You should have received a copy of the GNU General Public License
+!  along with this program; if not, write to the Free Software
+!  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+!  MA 02110-1301, USA.
+!  
+
+module preconditioner_mod
+  !* Author: Christopher MacMackin
+  !  Date: December 2016
+  !  License: GPLv3
+  !
+  ! Provides a type for preconditioning fields in an iterative
+  ! solver using Picard iteration.
+  !
+  use iso_fortran_env, only: r8 => real64
+  use factual_mod, only: scalar_field, vector_field, maxval, abs
+  use logger_mod, only: logger => master_logger
+  use jacobian_block_mod, only: jacobian_block
+  implicit none
+  private
+
+  type, public :: preconditioner
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! Uses Picard iterations to apply the inverse Jacobian of a system
+    ! to a vector, to low accuracy. Rather than directly computing the
+    ! inverse Jacobian, it is more efficient to approximate it. If
+    ! \(d\) is the vector being preconditioned, and \(z\) is the
+    ! result of applying the preconditioner, then $$ z = J^{-1}d
+    ! \Rightarrow Jz = d$$. Thus, the preconditioner can be applied by
+    ! approximately solving this system for \(z\). Linearising \(J\),
+    ! this system can be solved efficiently using Picard iteration.
+    !
+    ! Say that the Jacobian is an \(n\times n\) system of these blocks
+    ! (each labeled as \(J_{j,k}) and that the vector being
+    ! preconditioned constists of \(n\) scalar fields (\(d_j\)). Then
+    ! \(m^{th}\) estimate of the solution for the \(j^{th}\)
+    ! field in the preconditioned vector (\(z^m_j\)) is the solution
+    ! to $$ J_{j,j}z^m_j = d_j - \sum_{\substack{k=1\ k\ne j}}^n
+    ! J{j,k}z^{m-1}_k. $$ Depending on the type of fields being used
+    ! and the direction in which derivatives are being taken,
+    ! \(J_{j,j}\) may be tridiaganol, meaning it can be solved
+    ! efficiently. <!--Otherwise, it can be approximated that $$
+    ! J_{j,j}z^m_j = \left(\frac{\partial F}{\partial x_i} +
+    ! F\Delta_i\right)z^{m} \simeq \frac{\partial F}{\partial
+    ! x_i}z^m_j + F\frac{\partial z^{m-1}_j}{\partial x_i}. $$ The
+    ! first term in this approximation corresponds to a diagonal
+    ! matrix (which can be solved trivially), while the second term is
+    ! known and can be subtracted from the right-hand-side of the
+    ! linear system.-->
+    !
+    private
+    real(r8) :: tolerance      = 1.e-3_r8
+    integer  :: max_iterations = 20
+  contains
+    procedure :: apply => preconditioner_apply
+  end type preconditioner
+
+  interface preconditioner
+    module procedure constructor
+  end interface
+  
+contains
+
+  function constructor(tolerance, max_iterations) result(this)
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! Create a preconditioner object with the desired tolerance and
+    ! maximum number of iterations.
+    !
+    real(r8), optional, intent(in) :: tolerance
+      !! The tolerance within which to apply the inverse Jacobian.
+      !! Defaults to 0.001.
+    integer, optional, intent(in)  :: max_iterations
+      !! The maximum number of iterations to use when applying the
+      !! preconditioner. Defaults to 20.
+    type(preconditioner) :: this
+    if (present(tolerance)) this%tolerance = tolerance
+    if (present(max_iterations)) this%max_iterations = max_iterations
+  end function constructor
+
+  function preconditioner_apply(this, jacobian, vector) result(estimate)
+    !* Author: Chris MacMackin
+    !  Date: December 2016
+    !
+    ! Use Picard iteration to approximately multiply the state vector
+    ! by the inverse Jacobian. The details for this procedure are in
+    ! the documentation of the [[preconditioner(type)]] type.
+    !
+    class(preconditioner), intent(in)                    :: this
+    class(jacobian_block), dimension(:,:), intent(inout) :: jacobian
+    class(scalar_field), dimension(:), intent(in)        :: vector
+    class(scalar_field), dimension(:), allocatable       :: estimate
+
+    character(len=76), parameter :: success_format = '("Picard solver '// &
+         'converged with error of ",es8.5," after ",i0," iterations.")'
+    character(len=76), parameter :: failure_format = '("Picard solver '// &
+         'reached maximum (",i0,") iterations, with error ",es8.5,".")'
+    class(scalar_field), dimension(:), allocatable :: prev_estimate
+    integer                                        :: i, j, k, m, n
+    real(r8), dimension(:), allocatable            :: tmp
+    real(r8)                                       :: max_err
+    class(scalar_field), allocatable               :: tmp_field
+    logical                                        :: first
+    character(len=:), allocatable                  :: msg
+    
+    call logger%debug('preconditioner_apply','Entering function `precondition`.')
+    allocate(prev_estimate(size(vector)), mold=vector)
+    allocate(tmp_field, mold=vector(1))
+#ifdef DEBUG
+    if (size(jacobian,1) /= size(jacobian,2)) then
+      error stop('Jacobian is not a square matrix.')
+    end if
+    if (size(jacobian,1) /= size(vector)) then
+      error stop('Vector is of different size than Jacobian.')
+    end if
+#endif
+    n = size(vector)
+    ! Initialise prev_guess to 0
+    do concurrent (i=1:n)
+      call prev_estimate(i)%assign_meta_data(vector(i))
+      m = prev_estimate(i)%raw_size()
+      if (allocated(tmp)) then
+        if (size(tmp) /= m) then
+          deallocate(tmp)
+          allocate(tmp(m))
+          tmp = 0._r8
+        end if
+      else
+        allocate(tmp(m))
+        tmp = 0._r8
+      end if
+      call prev_estimate(i)%set_from_raw(tmp)
+    end do
+    ! Until reached maximum number of iterations...
+    do i = 1, this%max_iterations
+      ! For each row of the Jacobian, solve for the diagonal block,
+      ! with off-diagonal blocks applied to the previous guess and
+      ! subtracted from the right-hand-side.
+      max_err = 0._r8
+      allocate(estimate(size(vector)), mold=vector)
+      do j = 1, n
+        first = .true.
+        do k = 1, n
+          if (k == j) cycle
+          if (first) then
+            first = .false.
+            tmp_field = jacobian(j,k) * vector(k)
+          else
+            tmp_field = tmp_field + jacobian(j,k) * vector(k)
+          end if
+        end do
+        estimate(j) = jacobian(j,j)%solve_for(vector(j) - tmp_field)
+        max_err = max(max_err, &
+                      maxval(abs( (estimate(j)-prev_estimate(j))/(estimate(j)+1e-10_r8) )))
+      end do
+      ! If difference between result and previous guess is less than
+      ! the tolerance, stop iterations
+      if (max_err < this%tolerance) then
+        write(msg,success_format) max_err, this%max_iterations
+        call logger%debug('preconditioner_apply',msg)
+        call logger%debug('preconditioner_apply','Exiting function `precondition`.')
+        return
+      end if
+      deallocate(prev_estimate)
+      call move_alloc(estimate, prev_estimate)
+    end do
+    write(msg,failure_format) this%max_iterations, max_err
+    call logger%warning('preconditioner_apply',msg)
+    call logger%debug('preconditioner_apply','Exiting function `precondition`.')
+  end function preconditioner_apply
+  
+end module preconditioner_mod
